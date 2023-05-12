821950670d46e68039c900fade98b2233136eb6d
Tunahan ARIK <76400173+tunahanrk@users.noreply.github.com>
2023-05-12 21:51:40 +0300

    Files Uploaded

---
+++ Circle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp2
+{
+    internal class Circle:Shape2D
+    {
+        public double R { get; set; }
+        public Circle():base()
+        {
+            R = 0;
+            Name = "Circle";
+        }
+        public Circle(double r, double x, double y):base(x,y)
+        {
+            R = r;
+            Name = "Circle";
+        }
+        public double Circle_Area()
+        {
+            return 3.142 * R * R;
+        }
+        public double Circle_Prmtr()
+        {
+            return 2 * 3.142 * R;
+        }
+    }
+}
---
+++ Shape2D.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    internal class Shape2D : Shape
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public Shape2D() : base()
+        {
+            X = 0;
+            Y = 0;
+            Name = "2D Shape";
+        }
+
+        public Shape2D(double xx, double yy) : base()
+        {
+            X = xx;
+            Y = yy;
+            Name = "2D Shape";
+        }
+
+    }
+}
